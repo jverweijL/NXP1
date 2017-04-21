2b9a831d9bb94fc5089ed5c1eca023be2f43ff0a
Jan Verweij <Jan@Jan-MacBook-Pro.local>
2017-04-21 17:27:19 +0200

    added idea form

--- app/src/main/java/com/liferay/nxp1/IdeaActivity.java
+++ app/src/main/java/com/liferay/nxp1/IdeaActivity.java
@@ -7,8 +7,17 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.liferay.mobile.screens.ddl.form.DDLFormListener;
+import com.liferay.mobile.screens.ddl.model.DocumentField;
+import com.liferay.mobile.screens.ddl.model.Record;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+public class IdeaActivity extends AppCompatActivity implements DDLFormListener {
 
     private TextView mTextMessage;
 
@@ -27,10 +36,6 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
-                case R.id.navigation_idea:
-                    Intent intent = new Intent(getApplicationContext(), IdeaActivity.class);
-                    startActivity(intent);
-                    return true;
             }
             return false;
         }
@@ -40,11 +45,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_idea);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onDDLFormLoaded(Record record) {
+
+    }
+
+    @Override
+    public void onDDLFormRecordLoaded(Record record, Map<String, Object> valuesAndAttributes) {
+
+    }
+
+    @Override
+    public void onDDLFormRecordAdded(Record record) {
+        Toast.makeText(this,"Received information!",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void onDDLFormRecordUpdated(Record record) {
+
+    }
+
+    @Override
+    public void onDDLFormDocumentUploaded(DocumentField documentField, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    public void onDDLFormDocumentUploadFailed(DocumentField documentField, Exception e) {
+
+    }
+
+    @Override
+    public void error(Exception e, String userAction) {
+
+    }
 }
