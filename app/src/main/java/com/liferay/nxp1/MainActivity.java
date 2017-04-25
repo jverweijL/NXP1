151ef20eb5e3e4e4edd70addb15248a661b2e9ab
Victor Galan <victor.galan@liferay.com>
2017-04-25 16:27:07 +0200

    Configure main activity for receiving push notifications

--- app/src/main/java/com/liferay/nxp1/MainActivity.java
+++ app/src/main/java/com/liferay/nxp1/MainActivity.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import com.liferay.mobile.android.service.Session;
+import com.liferay.mobile.screens.push.PushScreensActivity;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends PushScreensActivity {
 
     private TextView mTextMessage;
+    private View content;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,6 +52,27 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        content = findViewById(android.R.id.content);
     }
 
+    @Override
+    protected Session getDefaultSession() {
+        return null;
+    }
+
+    @Override
+    protected void onPushNotificationReceived(JSONObject jsonObject) {
+        Log.d("PUSH", "Push received"+ jsonObject.toString());
+    }
+
+    @Override
+    protected void onErrorRegisteringPush(String message, Exception e) {
+        Snackbar.make(content, "Error registering device for push notifications", Snackbar.LENGTH_SHORT);
+    }
+
+    @Override
+    protected String getSenderId() {
+        return "1035449703602";
+    }
 }
