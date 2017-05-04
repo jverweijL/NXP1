58f438685b11ed79cc7fafd88f2777cfe8c4d1a8
Victor Galan <victor.galan@liferay.com>
2017-05-04 10:46:25 +0200

    Fix video fragment

--- app/src/main/java/com/liferay/nxp1/MainActivity.java
+++ app/src/main/java/com/liferay/nxp1/MainActivity.java
@@ -17,8 +17,13 @@ import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.push.Push;
 import com.liferay.mobile.screens.push.PushScreensActivity;
 import org.json.JSONObject;
-
-public class MainActivity extends PushScreensActivity implements RatingFragment.OnFragmentInteractionListener,IdeaFragment.OnFragmentInteractionListener,BlogFragment.OnFragmentInteractionListener {
+public class MainActivity extends PushScreensActivity implements   RatingFragment.OnFragmentInteractionListener,
+                                                                    IdeaFragment.OnFragmentInteractionListener,
+                                                                    BlogFragment.OnFragmentInteractionListener,
+                                                                    SelfserviceFragment.OnFragmentInteractionListener,
+                                                                    PaySlipFragment.OnFragmentInteractionListener,
+                                                                    ShiftChangeFragment.OnFragmentInteractionListener,
+                                                                    NewsFragment.OnFragmentInteractionListener {
   
     private View content;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
--- app/src/main/java/com/liferay/nxp1/MainFragment.java
+++ app/src/main/java/com/liferay/nxp1/MainFragment.java
@@ -1,21 +1,83 @@
 package com.liferay.nxp1;
 
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.VideoView;
+import com.liferay.mobile.screens.asset.AssetEntry;
+import com.liferay.mobile.screens.asset.display.AssetDisplayListener;
+import com.liferay.mobile.screens.dlfile.display.video.VideoDisplayScreenlet;
 
-public class MainFragment extends Fragment {
+import static android.content.Context.MODE_PRIVATE;
+
+public class MainFragment extends Fragment
+	implements AssetDisplayListener, MediaPlayer.OnCompletionListener {
+
+	private VideoDisplayScreenlet videoDisplayScreenlet;
+	private TextView textView;
 
 	public MainFragment() {
-		// Required empty public constructor
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 		Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_main, container, false);
+
+		View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+		videoDisplayScreenlet =
+			(VideoDisplayScreenlet) view.findViewById(R.id.asset_display_screenlet);
+		textView = (TextView) view.findViewById(R.id.no_video_textview);
+
+		if (isMandatoryVideoWatched()) {
+			videoDisplayScreenlet.setVisibility(View.GONE);
+			textView.setVisibility(View.VISIBLE);
+		} else {
+			videoDisplayScreenlet.setVisibility(View.VISIBLE);
+			textView.setVisibility(View.GONE);
+
+			videoDisplayScreenlet.load();
+			videoDisplayScreenlet.setListener(this);
+		}
+
+		return view;
+	}
+
+	@Override
+	public void onRetrieveAssetSuccess(AssetEntry assetEntry) {
+		VideoView videoView = (VideoView) getView().findViewById(R.id.liferay_video_asset);
+		videoView.setOnCompletionListener(this);
+	}
+
+	@Override
+	public void error(Exception e, String userAction) {
+		Log.d("", "");
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		videoDisplayScreenlet.setVisibility(View.GONE);
+		textView.setVisibility(View.VISIBLE);
+
+		setMandatoryVideoWatched();
+	}
+
+	public boolean isMandatoryVideoWatched() {
+		SharedPreferences sharedPreferences =
+			getActivity().getSharedPreferences("videoWatched", MODE_PRIVATE);
+		return sharedPreferences.getBoolean("isVideoWatched", false);
+	}
+
+	public void setMandatoryVideoWatched() {
+		SharedPreferences.Editor editor =
+			getActivity().getSharedPreferences("videoWatched", MODE_PRIVATE).edit();
+		editor.putBoolean("isVideoWatched", true);
+		editor.commit();
 	}
 }
